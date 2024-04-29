fa7dac59601054b016a9886120afa371131cc16d
Aastha <aasthamishra175@gmail.com>
2024-04-29 15:29:00 +0545

    Added some functionality to the buttons for the counter.

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -4,17 +4,30 @@ import './App.css'
 
 function App() {
 
+  let [counter, setCounter] = useState(0)
+
+
+  const decreaseValue = () => {
+    setCounter(counter--);
+  }
+  const resetValue = () => {
+    setCounter(counter = 0);
+  }
+  const increaseValue = () => {
+    setCounter(counter++);
+  }
 
   return (
     <>
       <p>
         <h1>The Counter</h1>
-        <h3>Counter Value : </h3>
+        <h3>Counter Value : {counter}</h3>
         <div className='btn'>
-        <button>Decrease</button>
-        <button>Increase</button>
+          <button onClick={decreaseValue}>Decrease</button>
+          <button onClick={resetValue}>Reset</button>
+          <button onClick={increaseValue}>Increase</button>
         </div>
-        
+
       </p>
 
     </>
