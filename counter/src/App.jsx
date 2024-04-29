735da425f50432880c7b8ba20f8656e1b6ce21da
Aastha <aasthamishra175@gmail.com>
2024-04-29 14:46:18 +0545

    A simple counter with react library.

---
+++ counter/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <p>
+        <h1>The Counter</h1>
+        <h3>Counter Value : </h3>
+        <div className='btn'>
+        <button>Decrease</button>
+        <button>Increase</button>
+        </div>
+        
+      </p>
+
+    </>
+  )
+}
+
+export default App
